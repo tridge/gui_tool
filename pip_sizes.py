6f8a568cd02444cacd420f689e7abedb8797f741
Buzz <davidbuzz@gmail.com>
2021-11-25 18:13:54 +1000

    more sizes and sort info

--- pip_sizes.py
+++ pip_sizes.py
@@ -15,11 +15,32 @@ def calc_container(path):
 
 dists = [d for d in pkg_resources.working_set]
 
+data = {}
+data2 = []
+data3 = {}
+
 for dist in dists:
     try:
         path = os.path.join(dist.location, dist.project_name)
         size = calc_container(path)
         if size/1000 > 1.0:
-            print (f"{dist}: {size/1000} KB")
+            #print (f"{dist}: {size/1000} KB")
+            data[size] = f"{dist}: {size/1000} KB"
+            a = f"{dist}"
+            data2.append(a.split()[0])# first word
+            data3[a.split()[0]] = f"{dist}: {size/1000} KB"
     except OSError:
         '{} no longer exists'.format(dist.project_name)
+
+sorted_dict = dict(sorted(data.items()))
+sorted_dict2 = sorted(data2)
+
+print("--------alpha-sorted-package-names-----------------")
+for i in sorted_dict2:
+    print(data3[i])
+
+#exit()
+print("\n--------size-sorted-package-info-----------------")
+for i in sorted_dict:
+    print(i,sorted_dict[i])
+	
